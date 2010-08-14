c19be9e197615185eff00942e8221437005d47db
TOGoS <togos00@gmail.com>
2010-08-13 22:24:43 -0500

    Implemented RegularRecordBlob#writeTo

--- src/togos/tzeu/RegularRecordBlob.java
+++ src/togos/tzeu/RegularRecordBlob.java
@@ -32,18 +32,6 @@ public abstract class RegularRecordBlob implements Blob
 	
 	public int read( long blobOffset, byte[] dest, int destOffset, int length ) throws IOException {
 		int rl = getRecordLength();
-		/*
-		int ri = (int)(blobOffset/rl);
-		long recordOffset;
-		long blobEndOffset = blobOffset+length;
-		byte[] buf = new byte[rl];
-		for( ; (recordOffset = ri*((long)rl)) < blobEndOffset; ++ri ) {
-			encodeRecord(ri, buf, 0);
-			
-		}
-		*/
-		
-		
 		int ri = ByteUtil.integer(blobOffset/rl);
 		if( ri >= items.size() ) return 0;
 		byte[] buf = new byte[rl];
@@ -58,6 +46,32 @@ public abstract class RegularRecordBlob implements Blob
 	}
 	
 	public long writeTo( long blobOffset, OutputStream os, long length ) throws IOException {
-		return 0;
+		int rl = getRecordLength();
+		int ri = ByteUtil.integer(blobOffset/rl);
+		if( ri >= items.size() ) return 0;
+		byte[] buf = new byte[rl];
+		long blobEndOffset = blobOffset+length;
+		if( blobEndOffset > rl*(long)items.size() ) blobEndOffset = rl*(long)items.size();
+		long recordOffset = ri*((long)rl);
+		int destOffset = (int)(recordOffset - blobOffset);
+		for( ; recordOffset < blobEndOffset; ++ri, recordOffset = ri*((long)rl) ) {
+			encodeRecord(ri, buf, 0);
+			
+			int srcOffset;
+			int cpyLength;
+			if( destOffset < 0 ) {
+				srcOffset = -destOffset;
+				cpyLength = rl - srcOffset;
+			} else {
+				srcOffset = 0;
+				cpyLength = rl;
+			}
+			if( recordOffset + cpyLength > blobEndOffset ) {
+				cpyLength = (int)(blobEndOffset-recordOffset);
+			}
+			os.write(buf, srcOffset, cpyLength );
+			destOffset = 0;
+		}
+		return blobEndOffset - blobOffset;
 	}
 }
--- test/togos/tzeu/RegularRecordBlobTest.java
+++ test/togos/tzeu/RegularRecordBlobTest.java
@@ -1,5 +1,6 @@
 package togos.tzeu;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -56,10 +57,53 @@ public class RegularRecordBlobTest extends TestCase
 		
 		byte[] buf = new byte[222];
 		int readed = tb.read(223, buf, 0, 222);
-		assertEquals(110, readed);
+		assertEquals(  110, readed   );
 		assertEquals(   31, buf[  0] );
 		assertEquals( -116, buf[109] );
 		assertEquals(    0, buf[110] );
 		assertEquals(    0, buf[220] );
+	}
+	
+	///
+	
+	public void testWriteToOnBoundary() throws IOException {
+		List items = new ArrayList();
+		items.add(new Integer(1)); items.add(new Integer(2)); items.add(new Integer(3));
+		Blob tb = new TestRecordBlob(items);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		tb.writeTo(0, baos, 222);
+		byte[] buf = baos.toByteArray();
+		assertEquals(   10, buf[  0] );
+		assertEquals(  120, buf[110] );
+		assertEquals(   20, buf[111] );
+		assertEquals( -126, buf[221] );
+	}
+
+	public void testWriteToOffBoundary() throws IOException {
+		List items = new ArrayList();
+		items.add(new Integer(1)); items.add(new Integer(2)); items.add(new Integer(3));
+		Blob tb = new TestRecordBlob(items);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		tb.writeTo(1, baos, 222);
+		byte[] buf = baos.toByteArray();
+		assertEquals(   11, buf[  0] );
+		assertEquals(  120, buf[109] );
+		assertEquals(   20, buf[110] );
+		assertEquals( -126, buf[220] );
+	}
+
+	public void testWriteToOffEnd() throws IOException {
+		List items = new ArrayList();
+		items.add(new Integer(1)); items.add(new Integer(2)); items.add(new Integer(3));
+		Blob tb = new TestRecordBlob(items);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		long readed = tb.writeTo(223, baos, 222);
+		byte[] buf = baos.toByteArray();
+		assertEquals(  110, readed   );
+		assertEquals(   31, buf[  0] );
+		assertEquals( -116, buf[109] );
 	}
 }
