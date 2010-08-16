c852780bed8279ae94560674cd186f500656e645
TOGoS <togos00@gmail.com>
2010-08-16 04:11:04 -0500

    Refactored ProcessCommand - now modeled as a sequence of filters

--- src/togos/tzeu/ProcessCommand.java
+++ src/togos/tzeu/ProcessCommand.java
@@ -5,33 +5,129 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ProcessCommand
 {
-	String infilename = null;
-	String mapname = null;
-	String outfilename = null;
+	class ArgumentError extends RuntimeException {
+		public ArgumentError(String message) { super(message); }
+		public ArgumentError(Throwable e) { super(e); }
+	}
 	
-	boolean compressSidedefs = false;
-	boolean preCompressSidedefs = false;
-	boolean listPreCompressSidedefs = false;
+	String mapname = null;
+	
+	List instructions = new ArrayList();
+	
+	interface Instruction {
+		public Object call( Object input );
+	}
+	class ReadLevelInstruction implements Instruction {
+		protected String mapname;
+		public ReadLevelInstruction( String mapname ) {
+			this.mapname = mapname;
+		}
+		public Object call( Object input ) {
+			if( !(input instanceof List) ) {
+				throw new ArgumentError("Require lump list to read level, got "+input.getClass());
+			}
+			try {
+				List lumps = (List)input;
+				LevelReader lr = new LevelReader();
+				return lr.readLevel(lumps, mapname);
+			} catch( IOException e ) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	class CompressSidedefsInstruction implements Instruction {
+		public Object call( Object input ) {
+			if( !(input instanceof Level) ) {
+				throw new ArgumentError("No level specified");
+			}
+			Level l = (Level)input;
+			SidedefCompressor sc = new SidedefCompressor();
+			SidedefCompressor.RemapResult pcr = sc.preCompress( l.sidedefs );
+			Level newLev = (Level)l.clone();
+			newLev.linedefs = sc.applyCompressionToLines(l.linedefs, pcr);
+			newLev.sidedefs = pcr.remappedItems;
+			System.err.println("-- Sidedef compression --");
+			System.err.println("Removed "+(l.sidedefs.size()-pcr.remappedItems.size())+" duplicate sidedefs.");
+			System.err.println(l.sidedefs.size()+" sidedefs originally, now "+pcr.remappedItems.size());
+			System.err.println(l.linedefs.size()+" linedefs");
+			return newLev;
+		}
+	}
+	class ReadWADInstruction implements Instruction {
+		protected String infilename;
+		public ReadWADInstruction( String infilename ) {
+			this.infilename = infilename;
+		}
+		public Object call( Object input ) {
+			if( infilename == null ) {
+				throw new ArgumentError("No infile specified");
+			}
+			File infile = new File(infilename);
+			if( !infile.exists() ) {
+				throw new ArgumentError("Input file '"+infilename+"' does not exist.");
+			}
+			if( !infile.canRead() ) {
+				throw new ArgumentError("Input file '"+infilename+"' is not readable.");
+			}
+			
+			try {
+				RandomAccessFileBlob wadBlob = new RandomAccessFileBlob(new RandomAccessFile(infile,"r"));
+				WADReader wr = new WADReader();
+				List l = wr.readLumps(wadBlob);
+				System.err.println("-- Read "+infilename+" --");
+				return l;
+			} catch( FileNotFoundException e ) {
+				throw new ArgumentError(e);
+			} catch( IOException e ) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	class WriteWADInstruction implements Instruction {
+		protected String outfilename;
+		public WriteWADInstruction( String outfilename ) {
+			this.outfilename = outfilename;
+		}
+		public Object call( Object input ) {
+			List lumps;
+			if( input instanceof List ) {
+				lumps = (List)input;
+			} else if( input instanceof Level ) {
+				LevelWriter lw = new LevelWriter();
+				lumps = lw.lumpify((Level)input);
+			} else {
+				throw new ArgumentError("Can't save non-level, non-lump-list: "+input.getClass());
+			}
+			try {
+				// todo: mkdirs?
+				FileOutputStream os = new FileOutputStream(outfilename);
+				WADWriter ww = new WADWriter();
+				ww.writeWad("PWAD", lumps, os);
+				os.close();
+				System.err.println("-- Wrote "+outfilename+" --");
+			} catch( IOException e ) {
+				throw new RuntimeException(e);
+			}
+			return input;
+		}
+	}
 	
 	public ProcessCommand( String[] args ) {
 		for( int i=0; i<args.length; ++i ) {
 			if( "-o".equals(args[i]) ) {
-				outfilename = args[++i];
+				instructions.add(new WriteWADInstruction(args[++i]));
 			} else if( "-map".equals(args[i]) ) {
-				mapname = args[++i];
+				instructions.add(new ReadLevelInstruction(args[++i]));
 			} else if( "-compress-sidedefs".equals(args[i]) ) {
-				compressSidedefs = true;
-			} else if( "-pre-compress-sidedefs".equals(args[i]) ) {
-				preCompressSidedefs = true;
-			} else if( "-list-pre-compress-sidedefs".equals(args[i]) ) {
-				preCompressSidedefs = true;
-				listPreCompressSidedefs = true;
+				instructions.add( new CompressSidedefsInstruction() );
 			} else if( args[i].length() == 0 || args[i].charAt(0) != '-' ) {
-				infilename = args[i];
+				instructions.add( new ReadWADInstruction(args[i]) );
 			} else {
 				throw new RuntimeException("Unrecognised argument: "+args[i]);
 			}
@@ -39,41 +135,9 @@ public class ProcessCommand
 	}
 	
 	public int run() {
-		if( infilename == null ) {
-			System.err.println("No infile specified");
-			System.exit(1);
-		}
+		Object stuff = null;
 		
-		LevelReader lr = new LevelReader();
-		
-		File infile = new File(infilename);
-		if( !infile.exists() ) {
-			System.err.println("Input file '"+infilename+"' does not exist.");
-			return 1;
-		}
-		if( !infile.canRead() ) {
-			System.err.println("Input file '"+infilename+"' is not readable.");
-			return 1;
-		}
-		RandomAccessFileBlob wad;
-		try {
-			wad = new RandomAccessFileBlob(new RandomAccessFile(infile,"r"));
-		} catch( FileNotFoundException e ) {
-			e.printStackTrace();
-			return 1;
-		}
-		
-		if( mapname == null ) {
-			System.err.println("No map name specified");
-			return 1;
-		}
-		Level l;
-		try {
-			l = lr.readLevel(wad, mapname);
-		} catch( IOException e ) {
-			throw new RuntimeException(e);
-		}
-		
+		/*
 		if( preCompressSidedefs ) {
 			SidedefCompressor sc = new SidedefCompressor();
 			SidedefCompressor.RemapResult pcr = sc.preCompress( l.sidedefs );
@@ -92,29 +156,11 @@ public class ProcessCommand
 				}
 			}
 		}
-		if( compressSidedefs ) {
-			SidedefCompressor sc = new SidedefCompressor();
-			SidedefCompressor.RemapResult pcr = sc.preCompress( l.sidedefs );
-			Level newLev = (Level)l.clone();
-			newLev.linedefs = sc.applyCompressionToLines(l.linedefs, pcr);
-			newLev.sidedefs = pcr.remappedItems;
-			System.err.println("-- Sidedef compression --");
-			System.err.println("Removed "+(l.sidedefs.size()-pcr.remappedItems.size())+" duplicate sidedefs.");
-			l = newLev;
-		}
+		*/
 		
-		if( outfilename != null && l != null ) {
-			LevelWriter lw = new LevelWriter();
-			List levLumps = lw.lumpify(l);
-			try {
-				// todo: mkdirs?
-				FileOutputStream os = new FileOutputStream(outfilename);
-				WADWriter ww = new WADWriter();
-				ww.writeWad("PWAD", levLumps, os);
-				os.close();
-			} catch( IOException e ) {
-				throw new RuntimeException(e);
-			}
+		for( Iterator it=instructions.iterator(); it.hasNext(); ) {
+			Instruction i = (Instruction)it.next();
+			stuff = i.call( stuff );
 		}
 		
 		return 0;
